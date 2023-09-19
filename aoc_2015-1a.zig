8d67adbe82c5911af5e75f0c3e857f4a4078aded
Stephen Krochenski <skrochen@krotek.com>
2023-09-19 16:57:31 -0400

    covert while loop to for loop

--- aoc_2015-1a.zig
+++ aoc_2015-1a.zig
@@ -3,19 +3,16 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    var fl: i16 = 0; // _floor _level
+    var fl: i16 = 0; // floor level
 
-    const dr = @embedFile("./aoc_2015-1.dat"); // _di_rections
+    const dr = @embedFile("./aoc_2015-1.dat"); // Santa's directions
 
-    var i: usize = 0; // _intruction count
-    // var it = std.mem.split(u8, floors, '');
-    while (i < dr.len) { // No Zig-ish way to do this ?????
-        if (dr[i] == '(') {
+    for (dr) |c| {
+        if (c == '(') {
             fl += 1;
-        } else if (dr[i] == ')') {
+        } else if (c == ')') {
             fl -= 1;
         }
-        i += 1;
     }
     try stdout.print("Santa ended up on floor: {d}\n", .{fl});
 }
